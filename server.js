bd63f605e5262577fb369edc89339103700f1f20
gyosen <u27a375e8cswzym@gmail.com>
2022-08-28 18:25:05 +0900

    listen port fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
